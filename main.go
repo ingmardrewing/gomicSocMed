b0bd7efa0911d8790d54cf122828a85349c50451
Ingmar Drewing <ingmar@drewing.de>
2018-05-02 12:31:47 +0200

    Add environmentally dependent logging

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path"
 
 	restful "github.com/emicklei/go-restful"
 	store "github.com/ingmardrewing/fsKeyValueStore"
 )
 
 func main() {
+	logfile := path.Join(env(GOMIC_PATH_TO_LOG), "log")
+	f, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
 	restful.Add(NewSocMedService())
 	store.Initialize()
 
@@ -25,7 +35,8 @@ func main() {
 
 		err := http.ListenAndServeTLS(":"+port, crt, key, nil)
 	*/
-	err := http.ListenAndServe(":"+port, nil)
+	log.Println("Listening on port", port, "via regular http")
+	err = http.ListenAndServe(":"+port, nil)
 
 	if err != nil {
 		log.Fatal(err.Error())
